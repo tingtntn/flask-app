5b388224bbc87cdce084d2039a3fab02e82ad67a
tingtntn <tingtntn@gmail.com>
2021-12-15 02:59:47 -0800

    small changes

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ CORS(app)
 @app.route('/', methods=['GET'])
 def index():
     req = requests.get('https://cat-fact.herokuapp.com/facts')
+    # data = json.loads(req.content)
+    # return render_template('index.html', cats=data)
     return req.content
 
 if __name__ == "__main__":
